7b9276baba978b8c11646dfc391d227ececb2de3
Joshua <jojo.asmo94@gmail.com>
2021-11-29 03:20:48 +0100

    Change helper functions from declarations to expressions

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,16 @@
-function computerPlay() {
+let computerPlay = function() {
     let options = ['Rock', 'Paper', 'Scissor'];
     return options[Math.floor(Math.random() * 3)];
 }
 
-function playerPlay()  {
+let playerPlay = function() {
     let selection;
     selection = prompt("Rock, paper or scissor? Press q to quit.").toLowerCase();
     while(!(selection === "rock" || selection === "paper" || selection === "scissor" || selection === "quit"))   selection = prompt("Invalid input!! Choose 'rock', 'paper' or 'scissor'!!").toLowerCase();
     return selection.replace(selection[0], selection[0].toUpperCase());
 }
 
-function playRound(playerSelection = "", computerSelection = "")  {
+let playRound = function(playerSelection = "", computerSelection = "")  {
     let win = `You win!! ${playerSelection} beats ${computerSelection}!!`
     let lose = `You lose!! ${playerSelection} gets beaten by ${computerSelection}!!`
     let hasPlayerWon = false;
@@ -45,7 +45,7 @@ function game() {
         let result = playRound(choice, computerPlay());
         if(result === "Player") playerScore++;
         else computerScore++;
-        console.log(`Score[player - CPU]:\t${playerScore} : ${computerScore}`);
+        console.log(`Score[player - CPU]\t${playerScore} : ${computerScore}`);
     }
 
     console.log("'Till next time!!");
